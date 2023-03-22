f24783dfa25ae7653abec94664f0038645a0eb97
Leonel-David-Navarro-Meneses <112946720+Leonel-David-Navarro-Meneses@users.noreply.github.com>
2023-03-22 11:49:07 -0600

    screends

--- lib/ui/screens/home_page_temp.dart
+++ lib/ui/screens/home_page_temp.dart
@@ -1,4 +1,5 @@
 import 'package:componentes/providers/menu_provider.dart';
+import 'package:componentes/ui/screens/alerts_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:componentes/utils/icons_mapping.dart';
 
@@ -29,17 +30,18 @@ Widget _lista() {
   // );
   return FutureBuilder(
     future: menuProvider.loadData(),
+    initialData: const [],
     builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
       //     print('builder: ');
       //   print(snapshot.data);
       return ListView(
-        children: _listaItems(snapshot.data as List<dynamic>),
+        children: _listaItems(snapshot.data as List<dynamic>, context),
       );
     },
   );
 }
 
-List<Widget> _listaItems(List<dynamic> data) {
+List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
   final List<Widget> opciones = [];
   IconMapping iconMap = const IconMapping();
 
@@ -52,7 +54,13 @@ List<Widget> _listaItems(List<dynamic> data) {
         color: Colors.blue,
       ),
       subtitle: Text(opt['texto2']),
-      onTap: () {},
+      onTap: () {
+        // final route = MaterialPageRoute(builder: (context) {
+        //   return const AlertsScreen();
+        // });
+        Navigator.pushNamed(context, opt['ruta']);
+        //    Navigator.push(context, route);
+      },
     );
     opciones
       ..add(tile)
