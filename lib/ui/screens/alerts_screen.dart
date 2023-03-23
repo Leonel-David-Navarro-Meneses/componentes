ad621e58bd2f51f7c5a284086c972505bdc2c771
Leonel-David-Navarro-Meneses <112946720+Leonel-David-Navarro-Meneses@users.noreply.github.com>
2023-03-23 11:29:43 -0600

    modificando las alertas

--- lib/ui/screens/alerts_screen.dart
+++ lib/ui/screens/alerts_screen.dart
@@ -3,11 +3,51 @@ import 'package:flutter/material.dart';
 class AlertsScreen extends StatelessWidget {
   const AlertsScreen({super.key});
 
+  void _mostrarAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        barrierColor: Colors.red,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: const Color.fromARGB(255, 255, 0, 0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            title: const Text('Titulo de la alerta'),
+            content: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('contenido de la alerta'),
+                FlutterLogo(
+                  size: 100.0,
+                )
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('aceptar')),
+              TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('cancelar')),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('Alerts Screen'),
+      appBar: AppBar(
+        title: const Text('alert screeen'),
+      ),
+      body: Center(
+        //RaisedButton era el boton anterior
+        child: ElevatedButton(
+          onPressed: () => _mostrarAlert(context),
+          child: const Text('Msotrar alertas'),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
