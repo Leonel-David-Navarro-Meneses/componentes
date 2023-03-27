d7fa2e54498f6232f82d03c7e6d39907ab672b53
Leonel-David-Navarro-Meneses <112946720+Leonel-David-Navarro-Meneses@users.noreply.github.com>
2023-03-27 14:01:38 -0600

    modificando input screen

--- lib/ui/screens/inputs_screen.dart
+++ lib/ui/screens/inputs_screen.dart
@@ -13,22 +13,67 @@ class _InputsScreenState extends State<InputsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('entrada de datos del usuario'),
+          title: const Text('Entrada de datos del usuario'),
         ),
         body: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
           children: [
-            _crearEntrada(),
+            _crearEntradaNombre(),
+            _crearEntradaPswd(),
           ],
         ));
   }
 
-  Widget _crearEntrada() {
+  Widget _crearEntradaNombre() {
     return TextField(
       autofocus: true,
-      textCapitalization: TextCapitalization.words,
+      textAlign: TextAlign.left,
+      textCapitalization: TextCapitalization.sentences,
+      style: const TextStyle(
+        color: Colors.redAccent,
+        fontWeight: FontWeight.bold,
+      ),
+      cursorColor: Colors.amberAccent,
+      cursorRadius: const Radius.circular(10.0),
+      cursorWidth: 5.0,
+      // maxLength: 4,
+      maxLines: null,
       decoration: InputDecoration(
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        hintText: "Escribe tu nombre ,Por favor",
+        labelText: "User Name: ",
+        // helperText: "Nombre",
+        prefixIcon: const Icon(Icons.person),
+        //iconColor: Colors.blueGrey,
+        //  prefix: const CircularProgressIndicator(),
+      ),
+      onChanged: (valor) {
+        _userName = valor;
+        print(_userName);
+      },
+    );
+  }
+
+  Widget _crearEntradaPswd() {
+    return TextField(
+      autofocus: true,
+      textAlign: TextAlign.left,
+      textCapitalization: TextCapitalization.sentences,
+      style: const TextStyle(
+        color: Colors.redAccent,
+        fontWeight: FontWeight.bold,
+      ),
+      cursorColor: Colors.amberAccent,
+      cursorRadius: const Radius.circular(10.0),
+      cursorWidth: 5.0,
+      maxLength: 8,
+      obscureText: true,
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        hintText: "Escribe tu contraseña ,Por favor",
+        labelText: "Password: ",
+        suffixIcon: const Icon(Icons.key),
       ),
       onChanged: (valor) {
         _userName = valor;
